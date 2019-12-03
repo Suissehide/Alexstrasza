f2df963ac59196a3ff5c4bb031b63510ad612041
leo.couffinhal1@gmail.com <leo.couffinhal@epitech.eu>
2019-12-03 19:21:15 +0100

    update

--- Symfony/src/Repository/ReponseRepository.php
+++ Symfony/src/Repository/ReponseRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Reponse;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Reponse|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ReponseRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Reponse::class);
     }
--- Symfony/src/Repository/SondageRepository.php
+++ Symfony/src/Repository/SondageRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Sondage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Sondage|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SondageRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Sondage::class);
     }
